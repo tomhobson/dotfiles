f592adcdce24704a97061d5b04e9e7702478909c
tomhobson <tomhobsonleeds@gmail.com>
2021-05-04 11:00:12 +0100

    added default editor and added ctrl left and right

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -34,8 +34,8 @@ setopt HIST_REDUCE_BLANKS
 #   like: git comm-[tab]
 setopt complete_aliases
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
 bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char

3ed9f389c32e3dfef7fb4993b2f03b1a62d99463
tomhobson <tomhobsonleeds@gmail.com>
2021-07-02 13:40:36 +0100

    added penrith

--- az/clusterAliases.zsh
+++ az/clusterAliases.zsh
@@ -9,8 +9,12 @@ getCluster()
   then
     command az aks get-credentials --subscription 5429b748-8754-45b3-bbab-036e0cc418ee --name jx3-mqube-build --resource-group mqubejx3build-cluster_rsg --admin
   fi
-    if [ "$1" = "jx3-prod" ]
+  if [ "$1" = "jx3-prod" ]
   then
     command az aks get-credentials --subscription 5429b748-8754-45b3-bbab-036e0cc418ee --name jx3-mqube-prod --resource-group mqubejx3prod-cluster_rsg --admin
+  fi
+  if [ "$1" = "jx3-penrith" ]
+  then
+    command az aks get-credentials --subscription 5429b748-8754-45b3-bbab-036e0cc418ee --name jx3-mqube-external-penrith --resource-group mqubejx3-external-penrith-cluster_rsg --admin
   fi
 }
